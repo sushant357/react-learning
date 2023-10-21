9cfeb89f6be7e063bf823b11736156cf6f6ef026
Sushant Ramchandra Jadhav <sushantjadhav357@gmail.com>
2023-10-21 12:44:25 +0530

    variables & datatypes

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = '2644';
+let accountUsername = 'Sushant Ramchandra Jadhav';
+var accountState = 'Mumbai';
+accountCity = 'Thane';
+
+/*
+Prefer not to use var 
+*/
+
+console.table([accountId,accountUsername,accountState, accountCity]);
---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";
+
+console.log(3+3);
+let name = "Sushant Jadhav";
+let number = 2;
+let applied = true;
+let amount = null;
+let totalAmount;
+
+/* Primitive Data Types */
+// number => 2
+// bigInt
+// string => ''
+// boolean => true/false
+// null => standalone value
+// undefined => value is not defined
+// symbol => unique
+
+// object
+console.log(typeof amount);
